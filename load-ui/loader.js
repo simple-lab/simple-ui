f42cc981d45752451df91cc987812e863ac3a989
MinKyu Kim <tomorDeveloper@gmail.com>
2019-04-08 16:55:03 +0900

    코드 라인 변경

--- load-ui/loader.js
+++ load-ui/loader.js
@@ -2,27 +2,21 @@
 
 /*
     진행 목록
-    1. 페이지 닫기
-    2. GET Parameter로 전달된 값을 페이지에 디스플레이
-    3. 사진 가져오기 버튼 클릭해서 내 디바이스의 이미지 가져오기
+    1. GET Parameter로 전달된 값을 페이지에 디스플레이
+    2. 사진 가져오기 버튼 클릭해서 내 디바이스의 이미지 가져오기
+    3. 페이지 닫기
 
     페이지 닫을 때 이동할 주소는 다음과 같습니다 :  uniwebview://MMNPCPage?Command=EXIT
     페이지 열 때 파라미터는 요런 식으로 : https://devcat.com?Token={UrlEncodedText}
 */
 
-// [1] 페이지 닫기
-var pageExitButton = document.getElementById("pageExitButton");
-pageExitButton.onclick = function () {
-    location.href="uniwebview://MMNPCPage?Command=EXIT";
-}
-
-// [2] GET Parameter로 전달된 값을 페이지에 디스플레이하기
+// [1] GET Parameter로 전달된 값을 페이지에 디스플레이하기
 var token = new URLSearchParams(location.search).get("Token");
 var dataLabel = document.getElementById("dataLabel");
 dataLabel.textContent = `Token: ${token}`;
 
 
-// [3] 사진 가져오기 버튼 클릭해서 내 디바이스의 이미지 가져오기
+// [2] 사진 가져오기 버튼 클릭해서 내 디바이스의 이미지 가져오기
 var fileDialog = document.createElement("input");
 fileDialog.type = "file";
 
@@ -41,3 +35,10 @@ var imageViewer = document.getElementById("imageViewer");
 loadImageButton.addEventListener("click", function(event) {
     fileDialog.click();
 }, false);
+
+
+// [3] 페이지 닫기
+var pageExitButton = document.getElementById("pageExitButton");
+pageExitButton.onclick = function () {
+    location.href="uniwebview://MMNPCPage?Command=EXIT";
+}
