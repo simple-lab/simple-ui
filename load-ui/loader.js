ddd572bb19fa78477f9f77e3d3eb14daeefe5781
MinKyu Kim <tomorDeveloper@gmail.com>
2019-04-08 15:05:19 +0900

    Add Test

---
+++ load-ui/loader.js
@@ -0,0 +1,35 @@
+"use strict;"
+
+/*
+    진행 목록
+    1. GET Parameter로 전달된 값을 페이지에 디스플레이
+    2. 사진 가져오기 버튼 클릭해서 내 디바이스의 이미지 가져오기
+    3. 페이지 닫기
+
+    페이지 닫을 때 이동할 주소는 다음과 같습니다 :  uniwebview://MMNPCPage?Command=EXIT
+    페이지 열 때 파라미터는 요런 식으로 : https://devcat.com?Token={UrlEncodedText}
+*/
+
+// [1] GET Parameter로 전달된 값을 페이지에 디스플레이하기
+var token = new URLSearchParams(location.search).get("Token");
+var dataLabel = document.getElementById("dataLabel");
+dataLabel.textContent = `Token: ${token}`;
+
+
+// [2] 사진 가져오기 버튼 클릭해서 내 디바이스의 이미지 가져오기
+var loadImageButton = document.getElementById("loadImageButton");
+var imageViewer = document.getElementById("imageViewer");
+
+var fileDialog = document.createElement("input");
+fileDialog.type = "file";
+
+loadImageButton.addEventListener("click", function() {
+    fileDialog.click();
+}, false);
+
+
+// [3] 페이지 닫기
+var pageExitButton = document.getElementById("pageExitButton");
+pageExitButton.onclick = function () {
+    location.href="uniwebview://MMNPCPage?Command=EXIT";
+}
